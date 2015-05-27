39ddf449a80cede079b0eedd8bc1ad867fc77204
hbomb <qiqi.zhou@yoho.cn>
2015-05-27 09:38:25 +0800

    代码整理，增加Pjax中间层

--- app.js
+++ app.js
@@ -2,110 +2,35 @@
 // builtin
 var fs = require('fs');
 
-// 3rd party
 var express = require('express');
-var hbs = require('hbs');
-var request = require('request');
-var _ = require('lodash');
-var bodyParser = require('body-parser');
 
-var userInterfaces = require('./interface/user');
+//中间件
+var hbs = require('hbs'); //handlebars视图插件
+var bodyParser = require('body-parser'); //body序列化插件
+var expressError = require('express-error'); //异常跟踪
+
 var app = express();
+var env = process.env.NODE_ENV || 'development'; //获取环境参数
+var proxyRoute = require('./lib/proxyRoute');
 
+//声明小部件目录
 hbs.registerPartial('partial', fs.readFileSync(__dirname + '/views/partial.hbs', 'utf8'));
 hbs.registerPartials(__dirname + '/views/partials');
 
-// set the view engine to use handlebars
+//设置handlebars视图引擎
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
+//body 序列化
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
+//静态目录
 app.use(express.static(__dirname + '/public'));
 
-function error(err, req, res, next) {
-   console.error(err.stack);
-   res.status(500);
-   res.send('Internal Server Error');
-}
-app.use(error);
-
-/**
- * 错误校验
- */
-function validate(req, params) {
-    var ret = {};
-    var message = '';
-    var error = false;
-    _.forEach(params, function (v) {
-        var val = req.params[v.name] || req.body[v.name];
-        var flag = false;
-        if (val) {
-            if (v.maxLength && v.minLength) {
-                if (val < v.maxLength + 1 && val > v.minLength - 1) {
-                    flag = true;
-                }
-            }
-            if (v.reg && v.reg.test(val)) {
-                flag = true;
-            }
-            if (flag === true) {
-                ret[v.name] = val;
-            }
-            else {
-                message += v.name + ':' + v.message + '\n';
-                error = true;
-            }
-        }
-    });
-    return {
-        params: ret,
-        message: message,
-        error: error
-    };
-}
-
-/**
- * 配置初始化接口路由
- * 
- */
-function parseConfig(userInterfaces) {
-    var domain = userInterfaces.domain;
-    _.forEach(userInterfaces.res, function (val) {
-        app[val.method.toLowerCase()](val.route, function (req, res) {
-            var ret = validate(req, val.params);
-            if (!ret.error) {
-                request({
-                    url: domain + val.url,
-                    method: val.method,
-                    qs: ret.params
-                }, function (error, response, body) {
-                        if (response && response.statusCode == 200) {
-                            res.send(body);
-                        } else {
-                            console.log('error: ' + (response&&response.statusCode));
-                            console.log(body);
-                            if(response){
-                                res.send('error: ' + response.statusCode);
-                            }
-                            else{
-                                throw new Error('server error!');
-                            }
-                        }
-                    });
-            }
-            else {
-                res.send(ret.message);
-            }
-        });
-    });
-}
-
-parseConfig(userInterfaces);
-
-app.get('/', function (req, res) {
-
+app.get('/', function(req, res) {
     res.locals = {
         some_value: 'foo bar',
         list: ['cat', 'dog']
@@ -114,7 +39,24 @@ app.get('/', function (req, res) {
     res.render('index');
 });
 
+/**
+ * 初始化代理路由
+ */
+proxyRoute.init(app, null, function(err) {
+    if (err) {
+        throw new Error(err);
+    }
+
+    if (env == 'development') {
+        app.use(expressError.express3({
+            contextLinesCount: 3,
+            handleUncaughtException: true,
+            title: 'YOWEB'
+        }));
+    };
+
+});
+
 
 
 app.listen(3000);
-
--- interface/user.js
+++ interface/user.js
@@ -1,19 +1,15 @@
-exports.domain = 'http://localhost';//http://uic.yoho.com
+exports.domain = 'http://localhost';
 exports.res =
-[
-	{
+	[{
 		route: '/user/:userid',
 		method: 'GET',
 		url: '/user',
-		params: [
-			{
-				name:'userid',
-				type: 'Number',
-				maxLength: '10',
-				minLength: '6',
-				reg: /^\d{6,10}$/,
-				message:'必须是6-10位的数字'
-			}
-		]
-	}
-];
+		params: [{
+			name: 'userid',
+			type: 'Number',
+			maxLength: '10',
+			minLength: '6',
+			reg: /^\d{6,10}$/,
+			message: '必须是6-10位的数字'
+		}]
+	}];
