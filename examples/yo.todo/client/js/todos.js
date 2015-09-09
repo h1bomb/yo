0eb1bc07466519d44c83ab3f855ebb739e16ef75
ZhouQiqi <qiqi.zhou@gmail.com>
2015-09-09 08:57:39 +0800

    编写处理事件模块

--- examples/yo.todo/client/js/todos.js
+++ examples/yo.todo/client/js/todos.js
@@ -18,6 +18,9 @@ function initPjax() {
     });
 }
 
+var ENTER_KEY = 13;
+var ESCAPE_KEY = 27;
+
 var $newTodoInput = $("#new-todo"), //新建一个todo
     $listLi = $("#todo-list li"), //列表单元
     $listToggle = $("#todo-list .togggle"), //切换todo状态
@@ -29,16 +32,25 @@ var $newTodoInput = $("#new-todo"), //新建一个todo
 
 
 var actions = {
-    toggleAll: {
-        url: '/todos/toggleall',
-        method: 'PUT'
+        toggleAll: {
+            url: '/todos/toggleall',
+            method: 'PUT',
+            eventHandle: [{
+                event: click,
+                elem: $toggleall
+            }]
+        }
     },
     add: {
         url: '/todo',
         method: 'POST',
         data: {
             todo: $newTodoInput.val()
-        }
+        },
+        eventHandle: [{
+            event: 'click',
+            elem: $newTodoInput
+        }]
     },
     edit: {
         url: '/todo/',
@@ -49,37 +61,70 @@ var actions = {
             this.data = {
                 todo: elem.find('label').text(),
                 state: state
-            }
+            };
         },
-        callback: function() {}
+        eventHandle: [{
+            event: 'click',
+            elem: $listToggle
+        }, {
+            event: 'dblclick',
+            elem: $listLi,
+            handle: function(e) {
+                var $input = $(e.target).closest('li').addClass('editing').find('.edit');
+                $input.val($input.val()).focus();
+                return false;
+            }
+        }, {
+            event: 'keyup',
+            elem: $listLiEdit,
+            handle: function(e) {
+                if (e.which === ENTER_KEY) {
+                    $(e.target).blur();
+                    return true;
+                }
+
+                if (e.which === ESCAPE_KEY) {
+                    $(e.target).blur();
+                }
+
+                return false;
+            }
+        }]
     },
     remove: {
-        url: '/todo',
+        url: '/todo/',
         method: 'DELETE',
-        ev: function() {},
-        callback: function() {}
+        ev: function(elem) {
+            this.url += elem.attr('data-id');
+        },
+        eventHandle: [{
+            event: click,
+            elem: $listDestroy
+        }]
     },
     clearCompleted: {
         url: '/todos/completed',
         method: 'DELETE',
-        ev: function() {},
-        callback: function() {}
-    },
-    completed: {
-        url: '/todo/',
-        method: 'PUT',
-        ev: function() {},
-        callback: function() {}
-    },
-    input: {
-        ev: function() {}
+        eventHandle: [{
+            event: click,
+            elem: $clearCompleted
+        }]
     }
 }
 
-function initArr() {
-
+function bind() {
+    $.each(actions, function(key, value) {
+        $.each(value.eventHandle, function(index, hb) {
+            hb.elem.on(hb.event, function(e) {
+                var isSend = false;
+                if (e.handle) {
+                    isSend = e.handle(e);
+                }
+            });
+        });
+    });
 }
 
-function send() {
+function reload() {
 
 }
